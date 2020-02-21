76ba8fd3b2f2cabd29ec7f19ce05f017b13f2cb7
mrmines85 <61034810+mrmines85@users.noreply.github.com>
2020-02-20 19:05:17 -0600

    Add files via upload
    Lab 2.5 MarcusM

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput1 = int.Parse(Console.ReadLine());
+            int userInput2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(userInput1 + userInput2);
+            Console.WriteLine(userInput1 - userInput2);
+            Console.WriteLine(userInput1 * userInput2);
+            Console.WriteLine(userInput1 / userInput2);
+
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello");
+            Console.WriteLine(userName);
+
+            int userInput3 = int.Parse(Console.ReadLine());
+            int userInput4 = int.Parse(Console.ReadLine());
+            int userInput5 = int.Parse(Console.ReadLine());
+            Console.WriteLine(userInput3 * userInput4 * userInput5);
+
+            int userAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("You look younger than " + userAge);
+        }
+    }
+}
